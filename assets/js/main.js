15979c99e77b05ab71b4afd79f4d53b93abbb8c5
Prapncaa <nindyekadarma@gmailcom>
2024-08-12 14:26:23 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+// // NAVIGATION BAR FUNCTION
+// function myMenuFunction(){
+//     var menuBtn = document.getElementById("myNavMenu");
+
+//     if(menuBtn.className === "nav-menu"){
+//         menuBtn.className += "responsive";
+
+//     } else {
+//         menuBtn.className = "nav-menu"; 
+//     }
+// }
+
+// // ADD SHADOW ON NAVIGATION BAR WHILE SCROLLLING
+// window.onscroll = function() {headerShadow()};
+
+// function headerShadow() {
+//     const navHeader =document.getElementById("header");
+
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         naavHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0.1";
+//         naavHeader.style.height = "70px";
+//         naavHeader.style.lineHeight = "70px";
+//     } else {
+
+//         navHeader.style.boxShadow = "none";
+//         navHeader.style.height = "90px";
+//         navHeader.style.lineHeight = "90px";
+//     }
+
+// }
+
+// // TYPING EFFECT
+
+// // ## SCROLL REVEAL ANIMATION
+
+//     // Home
+
+//     // Project Box
+
+//     // Headings
+
+// // ## SCROLL REVEAL LEFT_RIGHT ANIMATION
